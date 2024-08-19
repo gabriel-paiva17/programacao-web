c3ead2c70dddb29701e8cec53352289a709e1a5b
Gabriel Paiva <gabrielanpaiva2005@gmail.com>
2024-08-19 08:32:17 -0300

    using const for regex

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let confirmar = document.getElementById("confirmar");
 
 confirmar.addEventListener("click", submit);
 
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 function submit() {
     let nome = document.getElementById("nome").value;
     let idade = document.getElementById("idade").value;
     let email = document.getElementById("email").value;
-
-    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!emailRegex.test(email)) {
         alert("Por favor, insira um e-mail válido.");
